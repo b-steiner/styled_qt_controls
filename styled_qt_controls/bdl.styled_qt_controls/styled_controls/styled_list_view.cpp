b7de7ec250b67308f0d2a37ba8d5dd30f862e537
b-steiner <bsteiner@cg.tuwien.ac.at>
2016-08-29 15:56:41 +0200

    Added license header

--- styled_qt_controls/bdl.styled_qt_controls/styled_controls/styled_list_view.cpp
+++ styled_qt_controls/bdl.styled_qt_controls/styled_controls/styled_list_view.cpp
@@ -1,3 +1,22 @@
+/**************************************************************************************
+**
+** Copyright (C) 2016 Bernhard Steiner
+**
+** This file is part of the styled_qt_controls library
+**
+** This product is licensed under the GNU General Public License version 3.
+** The license is as published by the Free Software Foundation published at
+** https://www.gnu.org/licenses/gpl-3.0.html.
+**
+** Alternatively, this product is licensed under the GNU Lesser General Public
+** License version 3 for non-commercial use. The license is as published by the
+** Free Software Foundation published at https://www.gnu.org/licenses/lgpl-3.0.html.
+**
+** Feel free to contact us if you have any questions about licensing or want
+** to use the library in a commercial closed source product.
+**
+**************************************************************************************/
+
 #include <bdl.styled_qt_controls/styled_qt_controls.hpp>
 #include "styled_list_view.q.hpp"
 
